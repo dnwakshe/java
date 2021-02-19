9cc62322e0b21b820afcc71c58e09169d733413d
dnwakshe <wakshe2@gmail.com>
2021-02-20 02:17:31 +0530

    Basic functionality of file handling is added

--- FileMgt.java
+++ FileMgt.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 
 public class FileMgt {
+    
    public static void main(String[] args) {
-        int a;
+      
         System.out.println("Enter your choice..");
         System.out.println("1.Create File");
         System.out.println("2.Read File");
         Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-                switch(a){
-            case 1: files file1 = new files();  
-                   file1.WriteFile();
+        int choice = sc.nextInt(); 
+
+        files objMgt = new files(); // bojecte of files is created
+        
+                switch(choice){
+            case 1: objMgt.WriteFile(); // call for creating and wiritting in file 
                 break;
-            case 2: // ReadFile();
+            case 2: objMgt.ReadFile();  // call for reading form file. 
                 break;
             default:
                 System.out.println("wrong input..");
@@ -24,16 +27,14 @@ public class FileMgt {
 
     
 }
-class files {
+class files{
+
     
-    files() {
-        
-    }
     // This function is used for creating and writting in created file
     public void WriteFile(){
         try{
             
-            FileWriter myWriter = new FileWriter("file1.txt");
+            FileWriter myWriter = new FileWriter("file1.txt");  // creatting the file with file1 name
             myWriter.write("This is created through progream..!");
             myWriter.close();
             System.out.println("File is Sucessfully created..");
@@ -44,5 +45,25 @@ class files {
             e.printStackTrace();
 
         }
+    }
+
+    public void ReadFile(){
+
+        System.out.println("Enter file name want to read..");
+        Scanner sc = new Scanner(System.in); // scanner's object for get input form user.
+        String FileName = sc.next();
+        try {
+            File myObj = new File(FileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              System.out.println(data);
+            }
+            myReader.close();
+        } 
+            catch (FileNotFoundException e) {
+             System.out.println("An error occurred.");
+             e.printStackTrace();
+           }
     }
 }
